51f7129014a4505c0266f6bba130f273e9d279b5
henriquemrc12 <henriquemarcel2244@gmail.com>
2022-03-07 12:21:28 -0300

    Project created

---
+++ src/test/java/com/open/mscachemanager/MsCacheManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.mscachemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCacheManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
